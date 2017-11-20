78c23f2b032ce4a072fa19f2e893f2c3ef96f7de
SanjunLiu <sanjun_liu@sina.com>
2017-11-20 17:27:20 +0800

    add examples of data augment

---
+++ data_augment.sh
@@ -0,0 +1,8 @@
+python ../image_augmentor/main.py \
+    /apps/liusj/datasets/foodIDDataSets/trainSets_crop \
+    rot_3 rot_-3 rot_180 \
+    noise_0.01 noise_0.02 \
+    blur_2.0
+
+#blur_3.0
+    #trans_0_20 trans_20_0 trans_20_20 trans_-20_20 \
